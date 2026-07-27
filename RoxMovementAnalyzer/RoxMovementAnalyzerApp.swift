5eb58b69f918ce424a7e74da5cb9afb44dabfa0f
vikram.ho <vikram.ho@lalamove.com>
2026-07-27 11:03:00 +0800

    Initial Commit

---
+++ RoxMovementAnalyzer/RoxMovementAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoxMovementAnalyzerApp.swift
+//  RoxMovementAnalyzer
+//
+//  Created by Vikram Ho on 27/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct RoxMovementAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
